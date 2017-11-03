a4327a2efcb8040a1e7ea774f737822c56220546
Joe Zuntz <joezuntz@googlemail.com>
2017-11-03 15:01:45 +0000

    set version to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='descpipe',
-      version='1.0',
+      version='1.1',
       scripts=['bin/descpipe'],
       packages=['descpipe', 'descpipe.launcher'],
       )
