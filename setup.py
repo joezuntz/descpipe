5ac405e51ce5720dc26171a42b97ef9d0a8ce615
Joe Zuntz <joezuntz@googlemail.com>
2017-12-04 16:38:17 +0000

    update version number to 1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='descpipe',
-      version='1.2',
+      version='1.3',
       scripts=['bin/descpipe'],
       packages=['descpipe', 'descpipe.launcher'],
       install_requires=['pyyaml','py-dag']
